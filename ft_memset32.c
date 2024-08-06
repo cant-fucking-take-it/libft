bd6cc610b05fffa35ad236b5e93ec7a18f9614ab
Eina Safor Coma Bages <ecoma-ba@car4s5.42barcelona.com>
2024-08-06 11:12:03 +0200

    norm

--- ft_memset32.c
+++ ft_memset32.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 16:47:57 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/07/31 11:14:12 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/08/06 11:11:56 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
@@ -14,7 +14,7 @@
 
 void	*ft_memset32(void *ptr, int c, size_t n)
 {
-	size_t	i;
+	size_t		i;
 	uint32_t	*my_ptr;
 
 	if (!ptr)
